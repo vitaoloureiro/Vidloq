3d0267f13433e30ce3ddf30d112d74783a862e6a
victo <victo@DESKTOP-A9S2TTH>
2016-11-04 10:07:42 -0300

    Add project files.

---
+++ Vidloq/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidloq.Startup))]
+namespace Vidloq
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
